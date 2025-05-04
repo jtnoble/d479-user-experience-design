769f0a0a8fe47326b263eb23ccce5281561698fd
= <=>
2025-05-04 02:17:10 -0500

    added base_url for image pathing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import "../styles/home.css";
 import ImageAndCaption from "../components/ImageAndCaption";
 
+const BASE_URL = import.meta.env.BASE_URL;
+
 function Home() {
     return (
         <div>
-            <img id="overview-img" src="/images/island_home_img.jpg" alt="Image of island" />
+            <img id="overview-img" src={BASE_URL + "/images/island_home_img.jpg"} alt="Image of island" />
             <div className="container pt-5"> 
             <div className="text-center">
                 <h1>Welcome To Taniti</h1>
@@ -13,37 +15,37 @@ function Home() {
             
             <ImageAndCaption
                 leftAlign={true}
-                imgSrc={"/images/island_entertainment_img.jpg"}
+                imgSrc={BASE_URL + "/images/island_entertainment_img.jpg"}
                 content={{header: "Entertainment", content: "Test content to change later"}}
             ></ImageAndCaption>
 
             <ImageAndCaption
                 leftAlign={false}
-                imgSrc={"/images/island_lodging_img.jpg"}
+                imgSrc={BASE_URL + "/images/island_lodging_img.jpg"}
                 content={{header: "Lodging", content: "Test content to change later"}}
             ></ImageAndCaption>
 
             <ImageAndCaption
                 leftAlign={true}
-                imgSrc={"/images/island_entertainment_img.jpg"}
+                imgSrc={BASE_URL + "/images/island_entertainment_img.jpg"}
                 content={{header: "Transportation", content: "Test content to change later"}}
             ></ImageAndCaption>
 
             <ImageAndCaption
                 leftAlign={false}
-                imgSrc={"/images/island_lodging_img.jpg"}
+                imgSrc={BASE_URL + "/images/island_lodging_img.jpg"}
                 content={{header: "Grocery", content: "Test content to change later"}}
             ></ImageAndCaption>
 
             <ImageAndCaption
                 leftAlign={true}
-                imgSrc={"/images/island_entertainment_img.jpg"}
+                imgSrc={BASE_URL + "/images/island_entertainment_img.jpg"}
                 content={{header: "Restaurants", content: "Test content to change later"}}
             ></ImageAndCaption>
 
             <ImageAndCaption
                 leftAlign={false}
-                imgSrc={"/images/island_lodging_img.jpg"}
+                imgSrc={BASE_URL + "/images/island_lodging_img.jpg"}
                 content={{header: "Sightseeing", content: "Test content to change later"}}
             ></ImageAndCaption>
             </div>  
